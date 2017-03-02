bb4879f388a98927d9c485cd7e39ce153c1798ed
Taehyun Lim <taehyun.lim@gmail.com>
2017-03-01 16:47:34 -0800

    Add more interesting gaming factors

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,24 @@
 
+// CHARACTERS AS OBJECTS
 var z = {
         name: "z",
         isHero: false,
         isEnemy: true,
-        hp: 100,
-        dmg: 10,
+        hp: 115,
+        dmg: 8,
         becomeHero: function() {
         	this.isHero = true;
         	this.isEnemy = false;
         },
         attack: function(enemy) {
-        	enemy.hp = enemy.hp - this.dmg;
+            rate = this.rate;
+            instantDamage = this.dmg * getCritical(rate);
+            enemy.hp = enemy.hp - instantDamage;
+            return instantDamage;
         },
+        killCount: 0,
+        rate: 0,
+
 };
 
 var t = {
@@ -25,8 +32,13 @@ var t = {
         	this.isEnemy = false;
         },
         attack: function(enemy) {
-        	enemy.hp = enemy.hp - this.dmg;
+            rate = this.rate;
+            instantDamage = this.dmg * getCritical(rate);
+        	enemy.hp = enemy.hp - instantDamage;
+            return instantDamage;
         },
+        killCount: 0,
+        rate: 0,
 
 };
 
@@ -34,15 +46,20 @@ var p = {
         name: "p",
         isHero: false,
         isEnemy: true,
-        hp: 100,
-        dmg: 10,
+        hp: 90,
+        dmg: 12,
         becomeHero: function() {
         	this.isHero = true;	
         	this.isEnemy = false;
         },
         attack: function(enemy) {
-        	enemy.hp = enemy.hp - this.dmg;
+            rate = this.rate;
+            instantDamage = this.dmg * getCritical(rate);
+            enemy.hp = enemy.hp - instantDamage;
+            return instantDamage;
         },
+        killCount: 0,
+        rate: 0,
 
 };
 
@@ -50,31 +67,40 @@ var k = {
         name: "k",
         isHero: false,
         isEnemy: true,
-        hp: 100,
-        dmg: 10,
+        hp: 200,
+        dmg: 20,
         becomeHero: function() {
             this.isHero = true; 
             this.isEnemy = false;
         },
         attack: function(enemy) {
-            enemy.hp = enemy.hp - this.dmg;
+            rate = this.rate;
+            instantDamage = this.dmg * getCritical(rate);
+            enemy.hp = enemy.hp - instantDamage;
+            return instantDamage;
         },
+        killCount: 0,
+        rate: 0,
 
 };
 
+// Critical Hit Function
+function getCritical(rate) {
+    var num = Math.random();
+    if ( num < rate ){ return 2; } 
+    else { return 1; }
+}
 
+
+// Toggle Selected
 $( ".character" ).click(function() {
         $(this).toggleClass('selected').siblings().removeClass('selected');
 });
 
+// Select Hero
 $( "#selectHero").click(function() {
-        var heroElement = $( '.selected' );
-        $( "#hero-div" ).append(heroElement);
-        $( ".character").addClass('enemy');
-        $(this).hide();
-});
-
-$( "#selectHero").click(function() {
+    // First check if there is a toggle-selected character
+    if ( $( ".character.selected" )[0] ) {
         var heroElement = $( '.character.selected' );
         $( "#hero-div" ).append(heroElement);
         $( ".character").addClass('enemy');
@@ -82,21 +108,35 @@ $( "#selectHero").click(function() {
         $( "#hero-div .character" ).addClass('hero');
         $(this).hide();
         $( "#selectEnemy" ).show();
+        $( "#vs-div h3").show();
+    }
 });
 
+// Select Enemy
 $( "#selectEnemy" ).click(function() {
+    // First check if there is a toggle-selected character
+    if ( $( ".character.enemy.selected" )[0] ) {
         var enemyElement = $( '.character.enemy.selected' );
         $( "#enemy-div" ).append(enemyElement);
         $( "#enemy-div .character.enemy" ).addClass('currentEnemy');
+        $( "#attack").removeClass('disabled');
+        $( "#vs-div h3" ).text('VS');
         $(this).hide();
+        isNewEnemy = true;
+    }
 });
 
 
+// Set default global variables
 var heroActive = '';
 var enemyActive = '';
-var enemyCount = 3; // This is very hacky
+var enemyCount = 3; // This is kind of hacky
+var enemyOriginalHP = 0; // This is for regeneration after defeat
+var isNewEnemy = true; 
 
+// Attack Button
 $( "#attack" ).click(function() {
+    // Map hero selection to the matching object
     if ( $( "#hero-div .character.hero").attr("data-name") === 'p' ) {
             p.becomeHero();
             var heroActive = p;
@@ -111,6 +151,7 @@ $( "#attack" ).click(function() {
             var heroActive = k;
     } 
     
+    // Map enemey selection to the matching object
     if ( $( "#enemy-div .character.currentEnemy").attr("data-name") === 'p' ) {
             var enemyActive = p;
     } if ( $( "#enemy-div .character.currentEnemy").attr("data-name") === 'z' ) {
@@ -121,27 +162,63 @@ $( "#attack" ).click(function() {
             var enemyActive = k;
     } 
 
-    heroActive.attack(enemyActive);
-    heroActive.hp = heroActive.hp + 5;
-    enemyActive.attack(heroActive);
+    // Each time new enemy is selected:
+    if ( isNewEnemy ) {
+        heroActive.hp = heroActive.hp + 30;
+        enemyOriginalHP = enemyActive.hp;
+        isNewEnemy = false;
+    }
 
-    $( "*[data-name='" + heroActive.name +"'" ).find( "span.stats" ).text(" HP: " + heroActive.hp);
-    $( "*[data-name='" + enemyActive.name +"'" ).find( "span.stats" ).text(" HP: " + enemyActive.hp);
+    // Perform attack object method
+    var instantDmgHero = heroActive.attack(enemyActive);
+    var instantDmgEnemy = enemyActive.attack(heroActive);
 
+    // Create fading elements for damages dealt
+    var $instantHero = $( "<div>" ).html("-" + instantDmgHero).css("color", "red").fadeOut( "slow" );
+    var $instantEnemy = $( "<div>" ).html("-" + instantDmgEnemy).css("color", "red").fadeOut( "slow" );
+
+    // Print current stats on to the screen
+    $( "*[data-name='" + heroActive.name +"'" ).find( "span.stats" )
+        .html("Attack: " + Math.round(heroActive.dmg) 
+            + "</br> HP: " + Math.round(heroActive.hp) 
+            + "</br> Critical: " + ( Math.round(heroActive.rate *100) ) + "%" );
+    $( "*[data-name='" + enemyActive.name +"'" ).find( "span.stats" )
+        .html("Attack: " + Math.round(enemyActive.dmg) 
+            + "</br> HP: " + Math.round(enemyActive.hp) 
+            + "</br> Critical: " + ( Math.round(enemyActive.rate *100) ) + "%" );
+
+    //
+    $( "*[data-name='" + heroActive.name +"'" ).find( "span.stats" ).append($instantEnemy);
+    $( "*[data-name='" + enemyActive.name +"'" ).find( "span.stats" ).append($instantHero);
+
+    // For debugging purpose
     console.log(heroActive);
     console.log(enemyActive);
 
-    if ( heroActive.hp <= 0 ) {
-        $( "h1.text-center" ).text("You lost!");
+    // WHEN ALL ENEMIES ARE DYSTROED
+    if ( enemyCount === 1 && enemyActive.hp <= 0 ) {
+        $( "#vs-div h3" ).text("You won!");
+        $( "#selectEnemy" ).hide();
+        $( "#attack" ).hide();
     }
-    if ( enemyActive.hp <= 0 && enemyCount > 0 ) {
+    // WHEN HERO IS DESTROYED
+    else if ( heroActive.hp <= 0 ) {
+        $( "#vs-div h3" ).text("You lost!");
+        $( "#selectEnemy" ).hide();
+        $( "#attack" ).hide();
+    }
+    // WHEN CURRENT ENEMY IS DESTROYED
+    else if ( enemyActive.hp <= 0 && enemyCount > 0 ) {
+        heroActive.killCount++;
+        heroActive.dmg = heroActive.dmg + heroActive.killCount * (0.25 * enemyActive.dmg );
+        heroActive.hp = heroActive.hp + (0.5 * enemyOriginalHP);
+        heroActive.rate = heroActive.rate + (1/3);
+        $( "*[data-name='" + heroActive.name +"'" ).find( "span.stats" ).html(" Attack: " + heroActive.dmg + " </br> HP: " + heroActive.hp);
         $( ".character.currentEnemy" ).remove();
         enemyCount--;
         $( "#selectEnemy" ).show();
-    } if ( enemyActive.hp < 0 && enemyCount === 0 ) {
-        $( "h1.text-center" ).text("You won!");
-        $( "#selectEnemy" ).hide();
-        $( "#attack" ).hide();
+        $( "#vs-div h3" ).text("Pick your next enemey");
+        $( "#attack").addClass('disabled');
     }
 
 });
